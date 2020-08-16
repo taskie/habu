eaad2066dc50493fea64060d95c76361f7d17fc8
taskie <t@skie.jp>
2020-08-16 21:04:39 +0900

    add examples/features

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Generate configurations for [cobra](https://github.com/spf13/cobra) and [viper](
 
 ## Examples
 
-* [Source: examples/fwv/fwv.go](examples/fwv/fwv.go)
-    * [Generated: examples/fwv/options_habu.go](examples/fwv/options_habu.go)
+* [Source: examples/features/features.go](examples/features/features.go)
+    * [Generated: examples/features/options_habu.go](examples/features/options_habu.go)
 
 ## License
 
---
+++ examples/features/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+//go:generate habu -type Options -viper true
+
+type Options struct {
+	Long       string `hb_long:"some-long"`
+	Short      string `hb_long:"some-short" hb_short:"S"`
+	Usage      string `hb_long:"_" hb_short:"_" hb_usage:"Some usage"`
+	Env        string `hb_long:"_" hb_short:"_" hb_env:"SOME_ENV"`
+	Persistent string `hb_persistent:"true" hb_long:"_" hb_env:"_"`
+
+	String   string        `hb_long:"_" hb_value:"hello"`
+	Int      int           `hb_long:"_" hb_value:"42"`
+	Bool     bool          `hb_long:"_" hb_value:"true"`
+	Duration time.Duration `hb_long:"_" hb_value:"1h"`
+}
+
+func run(cmd *cobra.Command, args []string) {
+	options := Options{}
+	err := viper.Unmarshal(&options)
+	if err != nil {
+		log.Fatalf("can't parse options: %s", err.Error())
+	}
+	log.Printf("%+v", options)
+}
+
+func main() {
+	cmd := cobra.Command{
+		Use: "features",
+		Run: run,
+	}
+	viper.SetEnvPrefix("FEATURES")
+	PrepareOptionsFlags(&cmd, viper.GetViper())
+	err := cmd.Execute()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
